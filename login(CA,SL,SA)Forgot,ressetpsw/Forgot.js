1db0e9b37690cd166393b39ebec2a0189d807a77
kibboby <nrozunan@gmail.com>
2020-11-09 11:14:20 +0800

    Update Forgot.js

--- login(CA,SL,SA)Forgot,ressetpsw/Forgot.js
+++ login(CA,SL,SA)Forgot,ressetpsw/Forgot.js
@@ -1,13 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import {StackNavigator,} from 'react-navigation';
+import { StackNavigator, } from 'react-navigation';
 //import React from 'react';
-import { StyleSheet, Text, View, TextInput, Button,TouchableOpacity, ImageBackground} from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button, TouchableOpacity, ImageBackground } from 'react-native';
 import email from 'react-native-email';
 
 
 export default class Forgots extends Component {
- static navigationOptions = {
+  static navigationOptions = {
     title: 'Forgot Password',
     //headerShown:false,
     //Sets Header text of Status Bar
@@ -15,135 +15,135 @@ export default class Forgots extends Component {
 
 
   constructor() {
-        super();
-        this.state = {
-            FORGOT_email: '',
-            ActivityIndicator_Loading: false,
+    super();
+    this.state = {
+      FORGOT_email: '',
+      ActivityIndicator_Loading: false,
+    }
+  }
+
+  _resset() {
+    var x = "dasadas"
+    alert("sucessful resset " + x)
+  }
+
+  _resset_MySQL() {
+    const url = 'http://localhost:80/BAckend/forgot.php';
+    fetch(url,
+      {
+        method: 'POST',
+        headers:
+        {
+          'Origin': '*',
+          'Accept': 'application/json',
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(
+          {
+            email: this.state.FORGOT_email,
+          })
+
+      }).then((response) => response.json()).then((responseJsonFromServer) => {
+
+
+
+        if (responseJsonFromServer == "Invalid email") {
+          alert(responseJsonFromServer)
+          this.props.navigation.navigate('Login')
         }
-    }
 
-    _resset()
-    {
-    var x ="dasadas"
-    alert("sucessful resset "+x)
-    }
-
-    _resset_MySQL() {
-        const url = 'http://localhost:80/BAckend/forgot.php';
-        fetch( url,
-            {
-                method: 'POST',
-                headers:
-                {
-                   'Origin': '*',
-                   'Accept': 'application/json',
-                   'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(
-                    {
-                        email: this.state.FORGOT_email,
-                    })
-
-            }).then((response) => response.json()).then((responseJsonFromServer) => {
-
-            
-    
-     if(responseJsonFromServer=="Invalid email")
-     {
-     alert(responseJsonFromServer)
-     this.props.navigation.navigate('Login')
-     }
-
-     else{
-     ///////////
-      //const to = [this.state.FORGOT_email] // string or array of email addresses
-      const to = ["weedragon1999@gmail.com"] // string or array of email addresses
-       email(to, {
+        else {
+          ///////////
+          //const to = [this.state.FORGOT_email] // string or array of email addresses
+          const to = ["weedragon1999@gmail.com"] // string or array of email addresses
+          email(to, {
             //Optional additional arguments
             subject: 'Reset Password Sucessful',
-            body: 'Password reset to"Karuna"'})
-        //const sgMail =require("@sendgrid/mail");
-        //sgMail.setApiKey('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');
-        //const msg={
-            //to: "weedragon1999@gmail.com",
-            //from: "weedragon1999@gmail.com",
-            //subject:"Reset Password Successful",
-            //text:"Password reset to \"Karuna\""
-        //};
-        //sgMail.send(msg);
-    ////////////
-            alert(responseJsonFromServer)
-            this.props.navigation.navigate('Login')
-     }
+            body: 'Password reset to"Karuna"'
+          })
+          //const sgMail =require("@sendgrid/mail");
+          //sgMail.setApiKey('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');
+          //const msg={
+          //to: "weedragon1999@gmail.com",
+          //from: "weedragon1999@gmail.com",
+          //subject:"Reset Password Successful",
+          //text:"Password reset to \"Karuna\""
+          //};
+          //sgMail.send(msg);
+          ////////////
+          alert(responseJsonFromServer)
+          this.props.navigation.navigate('Login')
+        }
 
-            }).catch((error) => {
-                console.log(error);
-            });
-    }
- 
+      }).catch((error) => {
+        console.log(error);
+      });
+  }
 
-    
- render() {
-  return (
-    <View style={styles.container}>
-    {/* https://wallpaperaccess.com/full/1503970.jpg */}
-    <ImageBackground source={{uri: 'https://images.unsplash.com/photo-1562710057-b3a85fa9ba85?ixlib=rb-1.2.1&w=1000&q=80'}} style={styles.image}>
-    
 
-    <View style={styles.emailwhite}>
 
-    <View style={styles.usernameC}>
-      <Text style={styles.email}>User Email : </Text>
-      <TextInput 
-      style={styles.input}
-      placeholder='Email'
-      onChangeText={text => this.setState({ FORGOT_email:text})}
-       />
-    </View>
+  render() {
+    return (
+      <View style={styles.container}>
+        {/* https://wallpaperaccess.com/full/1503970.jpg */}
+        <ImageBackground source={{ uri: 'https://images.unsplash.com/photo-1562710057-b3a85fa9ba85?ixlib=rb-1.2.1&w=1000&q=80' }} style={styles.image}>
 
-   
-    <View>
-        <TouchableOpacity
-         style={styles.RessetButton}
-         onPress={() => this._resset_MySQL()}
-         //onPress={() => this._resset()}
-         >
-         <Text style={styles.Resset}>RESET</Text>
-     </TouchableOpacity>
-     </View>
-     <Text >{"email====>"+this.state. FORGOT_email }</Text>
-     </View>
 
-  
-      <StatusBar style="auto" />
-    </ImageBackground>
-    </View>
-     
-  );
-}}
+          <View style={styles.emailwhite}>
+
+            <View style={styles.usernameC}>
+              <Text style={styles.email}>User Email : </Text>
+              <TextInput
+                style={styles.input}
+                placeholder='Email'
+                onChangeText={text => this.setState({ FORGOT_email: text })}
+              />
+            </View>
+
+
+            <View>
+              <TouchableOpacity
+                style={styles.RessetButton}
+                onPress={() => this._resset_MySQL()}
+              //onPress={() => this._resset()}
+              >
+                <Text style={styles.Resset}>RESET</Text>
+              </TouchableOpacity>
+            </View>
+            <Text >{"email====>" + this.state.FORGOT_email}</Text>
+          </View>
+
+
+          <StatusBar style="auto" />
+        </ImageBackground>
+      </View>
+
+    );
+  }
+}
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:"grey",
+    backgroundColor: "grey",
     alignItems: 'center',
   },
 
   image: {
     flex: 1,
-    width:'100%',
-    height:'100%',
+    width: '100%',
+    height: '100%',
     resizeMode: "cover",
     justifyContent: "center"
 
   },
 
-  emailwhite:{
-  backgroundColor:"white",
-  padding: 50,
-  marginLeft: "18%",
-  marginRight: "18%",
-  alignItems:'center',
+  emailwhite: {
+    backgroundColor: "white",
+    padding: 50,
+    marginLeft: "18%",
+    marginRight: "18%",
+    alignItems: 'center',
   },
 
 
@@ -161,13 +161,13 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 10,
     padding: 10,
     fontSize: 20,
-    marginBottom:10,
-    marginTop:5,
-    width:200,
+    marginBottom: 10,
+    marginTop: 5,
+    width: 200,
     alignSelf: 'flex-start',
   },
 
- 
+
   RessetButton: {
     width: 200,
     marginTop: 20,
@@ -177,17 +177,17 @@ const styles = StyleSheet.create({
   },
 
   email: {
-  fontSize: 20,
-  fontWeight: "bold"
+    fontSize: 20,
+    fontWeight: "bold"
   },
 
-  
+
   Resset: {
     color: "white",
     fontSize: 20,
-    fontWeight:'bold',
+    fontWeight: 'bold',
     justifyContent: "center",
     textAlign: "center",
   },
-  
+
 });
